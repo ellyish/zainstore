f9bbabd70a86f6bda154e4273b226e3e38e0be03
ellyish <iraqgeeks@gmail.com>
2016-03-30 20:51:46 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,30 @@
+    var cart = [];    
+
+    var ractive = new Ractive({
+      // The `el` option can be a node, an ID, or a CSS selector.
+      el: '#container',
+
+      // We could pass in a string, but for the sake of convenience
+      // we're passing the ID of the <script> tag above.
+      template: '#template',
+
+      // Here, we're passing in some initial data
+      data: {cart: cart} 
+    });
+
+
+    ractive.on('addMoto', function (ctx) {
+        cart.push({item: "Moto X 360 Black", price: "200$"})
+    })
+
+    ractive.on('addGalaxy', function (ctx) {
+        cart.push({item: "Galaxy s7", price: "600$"})
+    })
+
+    ractive.on('addIphone', function (ctx) {
+        cart.push({item: "Apple iPhone 6p", price: "720$"})
+    })
+
+    ractive.on('addAdidas', function (ctx) {
+        cart.push({item: "Adidas Shoes", price: "120$"})
+    })
