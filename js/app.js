83a18404017645d90c2e6d9bb9bc4354279a09ac
Ali M. Ismail <aismail@solostudio.co>
2016-04-14 18:52:42 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -28,3 +28,49 @@
     ractive.on('addAdidas', function (ctx) {
         cart.push({item: "Adidas Shoes", price: 1000})
     })
+
+Array.prototype.sum = function (prop) {
+    var total = 0
+    for ( var i = 0, _len = this.length; i < _len; i++ ) {
+        total += this[i][prop]
+    }
+    return total
+}
+
+    ractive.on('checkout', function {
+        
+        openWindow({token: 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855',
+            amount: cart.sum("price")
+        })
+        
+    })
+    
+        var openWindow = function(params) {
+
+        var form = document.createElement("form");
+        form.setAttribute("method", "post");
+        form.setAttribute("action", "http://localhost:4040/pay");
+        form.setAttribute("target", "NewFile");
+        for (var i in params) {
+            if (params.hasOwnProperty(i)) {
+                var input = document.createElement('input');
+                input.type = 'hidden';
+                input.name = i;
+                input.value = params[i];
+                form.appendChild(input);
+            }
+        }
+
+
+        document.body.appendChild(form);
+        //note I am using a post.htm page since I did not want to make double request to the page
+        //it might have some Page_Load call which might screw things up.
+        window.open("post.htm", "NewFile", "width=1000, height=600, left=100, top=100, resizable=yes, scrollbars=yes");
+        form.submit();
+
+        document.body.removeChild(form);
+
+        
+
+
+    }
